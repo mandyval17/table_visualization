7586911968e88d523c5f8ca3bba8a7c56608113b
mandyval17 <mayurvalecha2603@gmail.com>
2025-05-06 09:33:46 +0530

    add Tailwind CSS integration and enhance table layout with cards

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,15 @@
 // import './App.css'
-import { Card } from 'primereact/card'
 import Table from './components/table'
 
 function App() {
 
   return (
     // <div style={{ display: 'flex', justifyContent: 'center', height: '100vh', alignItems: 'end' }}>
-    <Card style={{ width: '98%', boxShadow: '0 6px 12px rgba(0,0,0,0.32), 0 6px 12px rgba(0,0,0,0.46)' }}>
-      <Table />
-    </Card>
+    <div className='flex justify-center'>
+      <div className=' h-full w-3/4'>
+        <Table />
+      </div>
+    </div>
     // </div >
   )
 }
